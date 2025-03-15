5d3678049aeb2951c71b1d255e9132767cfc6a98
Kirubelyitagesu <125829088+Kirubelyitagesu@users.noreply.github.com>
2025-03-15 03:22:12 -0700

    Update Week1_Challenge_TwoSum.cpp

--- Week1_Challenge_TwoSum.cpp
+++ Week1_Challenge_TwoSum.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // iterate over each ellement 
         for (int i = 0; i < nums.size(); i++) {
-            // for each iteration, iterate for the next element to end
+            // for each iteration in i, iterate from the next element to end, to search for compliment that sums to give target
             for (int j = i + 1; j < nums.size(); j++) {
                 // then, check if nums[i]+nums[j] equals target, if yes print [i,j] if not continue 
                 if (nums[i] + nums[j] == target) {
